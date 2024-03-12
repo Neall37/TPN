36cd72b07126a3119eb0b6f6ac88930c0a2aa980
Sijun Wang <143745468+Neall37@users.noreply.github.com>
2024-03-11 23:09:32 -0400

    Update Create_dataset.py

--- TPN/Create_dataset.py
+++ TPN/Create_dataset.py
@@ -103,20 +103,26 @@ def process_species_data(df, h5_name, uni_length=20000, datatype='labeled',
     return positions_idx
 
 
-if __name__ == "__main__":
-    # Prepare your csv file containing species name, protein count and labels
-    df = pd.read_csv('labeled.csv')
-    step_size = 3400  # 5 files
+def create_dataset(file_path='labeled.csv', num_files=5, lb=5000, ub=22000,
+                   label_to_index_1 = {'hypha': 0, 'non_mycelium': 1},
+                   label_to_index_2 = {'Saprotrophs': 0, 'parasite': 1, 'Symbionts': 2, 'Pathogens': 3}):
+    step_size = (ub-lb)/num_files # 5 files
+    df = pd.read_csv(file_path) 
     idx = 0
-    for i in range(5000, 22000, step_size):
+    for i in range(lb, ub, step_size):
         ave = i+step_size
         # Filter DataFrame for ProteinCount between X and X+step_size
         filtered_df = df[(df['Protein_count'] >= i) & (df['Protein_count'] <= i+step_size)]
         # please change you label mapping accordingly
         positions_idx = process_species_data(df=filtered_df, uni_length=ave,
                                              h5_name=f"labeled_train_{i}.h5", datatype='labeled',
-                                             label_to_index_1 = {'hypha': 0, 'non_mycelium': 1},
-                                             label_to_index_2 = {'Saprotrophs': 0, 'parasite': 1, 'Symbionts': 2, 'Pathogens': 3})
+                                             label_to_index_1=label_to_index_1, label_to_index_2=label_to_index_2)
+    
+if __name__ == "__main__":
+    # Prepare your csv file containing species name, protein count and labels
+    create_dataset(file_path='labeled.csv', num_files=5, lb=5000, ub=22000, 
+                   label_to_index_1 = {'hypha': 0, 'non_mycelium': 1},
+                   label_to_index_2 = {'Saprotrophs': 0, 'parasite': 1, 'Symbionts': 2, 'Pathogens': 3})
 
 
 
